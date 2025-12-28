4be006b23c99b425667f2c06c542358680bd7e54
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 22:55:20 +0530

    adding the main file

--- src/main/java/spring_project2/spring_project2/App.java
+++ src/main/java/spring_project2/spring_project2/App.java
@@ -1,5 +1,11 @@
 package spring_project2.spring_project2;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dao.StudentDao;
+import com.model.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+        
+        StudentDao sd=context.getBean(StudentDao.class);
+        
+        Student s=new Student ();
+        
+        s.setName("Haridas Shinde");
+        s.setCity("pusad");
+        s.setPercentage(67.00);
+        
+        System.out.println(sd.insertStudent(s));
     }
 }
